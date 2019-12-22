77568300e373a67c1ed459e07ce2e6195df9c561
Vishal Khandelwal <vishalkhandelwaal@gmail.com>
2019-12-22 23:42:34 +0530

    added files

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import "./App.css";
+import Posts from "./components/Posts";
+import Postform from "./components/Postform";
+import store from "./store";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <Postform />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
